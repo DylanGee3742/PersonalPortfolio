831140d59df2b4d4b3f26c1549d3408fb5b8cb02
Dylan Gee <dylangee@gmail.com>
2022-07-02 13:02:31 +0100

    More styling added to night mode setting

--- interactivity.js
+++ interactivity.js
@@ -3,13 +3,18 @@ let button = document.querySelector('button');
 
 
 function nightModeClicker() {
-  body.style.backgroundColor = '#121212';
+  body.style.backgroundColor = '#050e30';
   body.style.color = 'white';
   document.getElementById('mysterious-organism').style.color = 'white';
   document.getElementById('my-own-company').style.color = 'white';
   document.getElementById('credit-card-checker').style.color = 'white';
   document.getElementById('fotomatic').style.color = 'white';
   document.getElementById('holiday-planner').style.color = 'white';
+  document.getElementById('about-me').style.color = 'white';
+  document.getElementById('projects').style.color = 'white';
+  document.getElementById('skills').style.color = 'white';
+  document.getElementById('contact-me').style.color = 'white';
+  document.getElementById('header').style.borderBottom = '1px solid white';
 }
 
 button.onclick = nightModeClicker;
