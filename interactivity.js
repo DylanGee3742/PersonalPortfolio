e4f2091092f01aa116a3b374d1f0f7a5dbd9fe3c
Dylan Gee <dylangee@gmail.com>
2022-07-03 14:20:05 +0100

    Styling finished for night mode button

--- interactivity.js
+++ interactivity.js
@@ -15,6 +15,26 @@ function nightModeClicker() {
   document.getElementById('skills').style.color = 'white';
   document.getElementById('contact-me').style.color = 'white';
   document.getElementById('header').style.borderBottom = '1px solid white';
+  document.querySelector('table').style.backgroundColor = 'white';
+  document.getElementById('name').style.color = 'white';
+  document.getElementById('dylan').style.color = 'white';
+  document.getElementById('age').style.color = 'white';
+  document.getElementById('23').style.color = 'white';
+  document.getElementById('qualifications').style.color = 'white';
+  document.getElementById('results').style.color = 'white';
+  document.getElementById('name').style.backgroundColor = '#050e30';
+  document.getElementById('dylan').style.backgroundColor = '#050e30';
+  document.getElementById('age').style.backgroundColor = '#050e30';
+  document.getElementById('23').style.backgroundColor = '#050e30';
+  document.getElementById('qualifications').style.backgroundColor = '#050e30';
+  document.getElementById('name-row').style.backgroundColor = '#050e30';
+  document.getElementById('name-result').style.backgroundColor = '#050e30';
+  document.getElementById('age-row').style.backgroundColor = '#050e30';
+  document.getElementById('age-result').style.backgroundColor = '#050e30';
+  document.getElementById('qualifications-row').style.backgroundColor = '#050e30';
+  document.getElementById('qualifications-result').style.backgroundColor = '#050e30';
+  document.querySelector('button').innerHTML = 'Day Mode!'
+
 }
 
 button.onclick = nightModeClicker;
