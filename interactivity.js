792db10abca53d531ed4bc4402ec45832ee223fa
Dylan Gee <dylangee@gmail.com>
2022-07-04 16:45:30 +0100

    Day mode button hmtl styling and functionaility added

--- interactivity.js
+++ interactivity.js
@@ -1,10 +1,12 @@
 let body = document.querySelector('body');
-let button = document.querySelector('button');
+let nightButton = document.getElementById('night-mode-button');
+let dayButton = document.getElementById('day-mode-button');
 
 
 function nightModeClicker() {
   body.style.backgroundColor = '#050e30';
   body.style.color = 'white';
+  body.classList.toggle("dark-mode");
   document.getElementById('mysterious-organism').style.color = 'white';
   document.getElementById('my-own-company').style.color = 'white';
   document.getElementById('credit-card-checker').style.color = 'white';
@@ -33,8 +35,42 @@ function nightModeClicker() {
   document.getElementById('age-result').style.backgroundColor = '#050e30';
   document.getElementById('qualifications-row').style.backgroundColor = '#050e30';
   document.getElementById('qualifications-result').style.backgroundColor = '#050e30';
-  document.querySelector('button').innerHTML = 'Day Mode!'
 
 }
 
-button.onclick = nightModeClicker;
+nightButton.onclick = nightModeClicker;
+
+function dayModeClicker() {
+  body.style.backgroundColor = 'white';
+  body.style.color = 'black';
+  document.getElementById('mysterious-organism').style.color = 'black';
+  document.getElementById('my-own-company').style.color = 'black';
+  document.getElementById('credit-card-checker').style.color = 'black';
+  document.getElementById('fotomatic').style.color = 'black';
+  document.getElementById('holiday-planner').style.color = 'black';
+  document.getElementById('about-me').style.color = 'black';
+  document.getElementById('projects').style.color = 'black';
+  document.getElementById('skills').style.color = 'black';
+  document.getElementById('contact-me').style.color = 'black';
+  document.getElementById('header').style.borderBottom = '1px solid black';
+  document.querySelector('table').style.backgroundColor = 'black';
+  document.getElementById('name').style.color = 'black';
+  document.getElementById('dylan').style.color = 'black';
+  document.getElementById('age').style.color = 'black';
+  document.getElementById('23').style.color = 'black';
+  document.getElementById('qualifications').style.color = 'black';
+  document.getElementById('results').style.color = 'black';
+  document.getElementById('name').style.backgroundColor = 'white';
+  document.getElementById('dylan').style.backgroundColor = 'white';
+  document.getElementById('age').style.backgroundColor = 'white';
+  document.getElementById('23').style.backgroundColor = 'white';
+  document.getElementById('qualifications').style.backgroundColor = 'white';
+  document.getElementById('name-row').style.backgroundColor = 'white';
+  document.getElementById('name-result').style.backgroundColor = 'white';
+  document.getElementById('age-row').style.backgroundColor = 'white';
+  document.getElementById('age-result').style.backgroundColor = 'white';
+  document.getElementById('qualifications-row').style.backgroundColor = 'white';
+  document.getElementById('qualifications-result').style.backgroundColor = 'white';
+}
+
+dayButton.onclick = dayModeClicker;
